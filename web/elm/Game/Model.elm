d2800939d45e262c0d30ea7392b5b73e1c1f0ba1
Ricardo García Vega <bigardone@gmail.com>
2016-09-12 08:53:04 +0200

    Converted board grid to dict

--- web/elm/Game/Model.elm
+++ web/elm/Game/Model.elm
@@ -1,5 +1,7 @@
 module Game.Model exposing (..)
 
+import Dict
+
 
 type alias Model =
     { game : Game
@@ -65,113 +67,9 @@ type alias Coordinates =
 type alias Board =
     { player_id : String
     , ships : Maybe (List Ship)
-    , grid : Grid
+    , grid : Dict.Dict String String
     , ready : Bool
     , hit_points : Int
-    }
-
-
-type alias Grid =
-    { c00 : String
-    , c01 : String
-    , c02 : String
-    , c03 : String
-    , c04 : String
-    , c05 : String
-    , c06 : String
-    , c07 : String
-    , c08 : String
-    , c09 : String
-    , c10 : String
-    , c11 : String
-    , c12 : String
-    , c13 : String
-    , c14 : String
-    , c15 : String
-    , c16 : String
-    , c17 : String
-    , c18 : String
-    , c19 : String
-    , c20 : String
-    , c21 : String
-    , c22 : String
-    , c23 : String
-    , c24 : String
-    , c25 : String
-    , c26 : String
-    , c27 : String
-    , c28 : String
-    , c29 : String
-    , c30 : String
-    , c31 : String
-    , c32 : String
-    , c33 : String
-    , c34 : String
-    , c35 : String
-    , c36 : String
-    , c37 : String
-    , c38 : String
-    , c39 : String
-    , c40 : String
-    , c41 : String
-    , c42 : String
-    , c43 : String
-    , c44 : String
-    , c45 : String
-    , c46 : String
-    , c47 : String
-    , c48 : String
-    , c49 : String
-    , c50 : String
-    , c51 : String
-    , c52 : String
-    , c53 : String
-    , c54 : String
-    , c55 : String
-    , c56 : String
-    , c57 : String
-    , c58 : String
-    , c59 : String
-    , c60 : String
-    , c61 : String
-    , c62 : String
-    , c63 : String
-    , c64 : String
-    , c65 : String
-    , c66 : String
-    , c67 : String
-    , c68 : String
-    , c69 : String
-    , c70 : String
-    , c71 : String
-    , c72 : String
-    , c73 : String
-    , c74 : String
-    , c75 : String
-    , c76 : String
-    , c77 : String
-    , c78 : String
-    , c79 : String
-    , c80 : String
-    , c81 : String
-    , c82 : String
-    , c83 : String
-    , c84 : String
-    , c85 : String
-    , c86 : String
-    , c87 : String
-    , c88 : String
-    , c89 : String
-    , c90 : String
-    , c91 : String
-    , c92 : String
-    , c93 : String
-    , c94 : String
-    , c95 : String
-    , c96 : String
-    , c97 : String
-    , c98 : String
-    , c99 : String
     }
 
 
