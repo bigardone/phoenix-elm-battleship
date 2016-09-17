ced23573229325ad749ec0e2258d6a50fe219904
Ricardo García Vega <bigardone@gmail.com>
2016-09-17 20:14:03 +0200

    Fixed title and description

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -7,6 +7,6 @@ if (elmDiv) {
   const app = Elm.Main.embed(elmDiv, { playerId: playerId, baseUrl: document.origin });
 
   app.ports.setDocumentTitle.subscribe((title) => {
-    document.title = `${title} · Phoenix Battleship`;
+    document.title = `${title} · Phoenix & Elm Battleship`;
   });
 }
