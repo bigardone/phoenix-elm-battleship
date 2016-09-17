4309500a7c4181ea3756b469aea460527a8700fb
Ricardo García <bigardone@gmail.com>
2016-09-17 10:04:35 +0200

    Removed commented code

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,22 +1,3 @@
-/*
-import React                    from 'react';
-import ReactDOM                 from 'react-dom';
-import { browserHistory }       from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import configureStore           from './store';
-import Root                     from './containers/root';
-
-
-
-const store  = configureStore(browserHistory);
-const history = syncHistoryWithStore(browserHistory, store);
-
-const target = document.getElementById('main_container');
-const node = <Root routerHistory={history} store={store}/>;
-
-ReactDOM.render(node, target);
-*/
-
 import Elm from './main';
 
 const playerId = window.playerId;
